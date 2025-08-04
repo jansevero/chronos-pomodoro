7f79d4a9c1a25d65854c73afe0d0d9b786f5e53c
Jan Severo <jansevero@gmail.com>
2025-08-04 20:56:18 -0300

    aula 19

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
-import { Heading } from './components/Heading';
-
 import './styles/theme.css';
 import './styles/global.css';
-import { TimerIcon } from 'lucide-react';
 
 export default function App() {
 
   return (
     <>
-      <Heading>
-         Hello World!
-         <button>
-          <TimerIcon />
-         </button>
-      </Heading>
+      <div className="container">
+        <div className="content">
+          <section>
+            LOGO
+          </section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>
+            MENU
+          </section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>
+            FORM
+          </section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>
+            FOOTER
+          </section>
+        </div>
+      </div>
     </>
   )
 }
