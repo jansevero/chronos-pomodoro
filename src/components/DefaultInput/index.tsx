2af9a2802cd6e4ad3de9b272a4b4e7ca4d196b52
Jan Severo <jansevero@gmail.com>
2025-08-09 14:20:35 -0300

    aula 27

--- src/components/DefaultInput/index.tsx
+++ src/components/DefaultInput/index.tsx
@@ -1,12 +1,14 @@
 type DefaultInputProps = {
   id: string;
+  labelText?: string; //opcional
 } & React.ComponentProps<'input'>;
 
-export function DefaultInput({ id,type }: DefaultInputProps) {
+export function DefaultInput({ id, type, labelText, ...rest }: DefaultInputProps) {
   return (
-    <>
-      <label htmlFor={ id }>task</label>
-      <input id={ id } type={ type } />
+    <> 
+      {/* mesmo que ? : sem o : */}
+      {labelText && <label htmlFor={ id }>{ labelText }</label>}
+      <input id={ id } type={ type } {...rest} />
     </>
   );
 }
