e5cc2049a53c1148bcdf9fa9d47893e56baa0e87
Jan Severo <jansevero@gmail.com>
2025-08-13 00:00:31 -0300

    aula 32

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,28 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { Countdown } from './components/Countdown';
-
-import './styles/theme.css';
-import './styles/global.css';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { PlayCircleIcon } from 'lucide-react';
 import { Footer } from './components/Footer';
 
+import './styles/theme.css';
+import './styles/global.css';
+import { Heading } from './components/Heading';
+import { useState } from 'react';
+
 export default function App() {
+  const [numero, setNumero] = useState(0);
+
+  function handleClick() {
+    setNumero(numero + 1);
+  }
 
   return (
     <>
+      <Heading>Número: {numero}</Heading>
+      <button onClick={handleClick}>Olá</button>
       <Container>
         <Logo />
       </Container>
@@ -31,10 +40,10 @@ export default function App() {
         <form className='form' action="">
           <div className='formRow'>
             <DefaultInput
-              labelText='task'
+              labelText={numero.toString()}
               id='meuInput'
               type='text'
-              placeholder='Digite algo'/>
+              placeholder='Digite algo' />
           </div>
 
           <div className='formRow'>
@@ -49,7 +58,6 @@ export default function App() {
 
           <div className="formRow">
             <DefaultButton icon={<PlayCircleIcon />} color='green' />
-            <DefaultButton icon={<StopCircleIcon />} color='red' />
           </div>
         </form>
       </Container>
