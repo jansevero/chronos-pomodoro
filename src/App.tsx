da322ce211e2bfe499a950dffbcebb0781f79d64
Jan Severo <jansevero@gmail.com>
2025-08-13 22:18:55 -0300

    aula 39

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,8 @@
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { Countdown } from './components/Countdown';
-import { DefaultInput } from './components/DefaultInput';
-import { Cycles } from './components/Cycles';
-import { DefaultButton } from './components/DefaultButton';
-import { PlayCircleIcon } from 'lucide-react';
-import { Footer } from './components/Footer';
 
+import Home from './pages/Home';
 import './styles/theme.css';
 import './styles/global.css';
 
 export default function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <Countdown />
-      </Container>
-
-      <Container>
-        <form className='form' action="">
-          <div className='formRow'>
-            <DefaultInput
-              labelText={'task'}
-              id='meuInput'
-              type='text'
-              placeholder='Digite algo' />
-          </div>
-
-          <div className='formRow'>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-            </p>
-          </div>
-
-          <div className="formRow">
-            <Cycles />
-          </div>
-
-          <div className="formRow">
-            <DefaultButton icon={<PlayCircleIcon />} color='green' />
-          </div>
-        </form>
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  )
+  return <Home />
 }
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,12 @@
+import { Container } from "../../components/Container";
+import MainTemplate from "../../templates/MainTemplate";
+
+export default function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <h1>404 Página não encontrada</h1>
+      </Container>
+    </MainTemplate>
+  )
+}
