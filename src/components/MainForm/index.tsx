ed652132655b3b957e82e3e0fcc3329ef8223bea
Jan Severo <jansevero@gmail.com>
2025-08-23 13:12:27 -0300

    Aula 68

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -8,6 +8,7 @@ import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
 import { getNextCycle } from "../../utils/getNextCycle";
 import { getNextCycleType } from '../../utils/getNextCycleType';
 import { TaskActionTypes } from "../../contexts/TaskContext/taskActions";
+import { Tips } from "../Tips";
 
 export function MainForm() {
   const { state, dispatch } = useTaskContext();
@@ -60,9 +61,7 @@ export function MainForm() {
       </div>
 
       <div className='formRow'>
-        <p>
-          O próximo intervalo é de 25 minutos
-        </p>
+        <Tips />
       </div>
 
       {state.currentCycle > 0 && (
