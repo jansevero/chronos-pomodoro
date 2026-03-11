0ce66d38104fbe1af928f330a9f991ac7c9da1eb
Jan Severo <jansevero@gmail.com>
2026-03-11 19:10:56 -0300

    Aula 84

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -6,6 +6,7 @@ import { MainTemplate } from '../../templates/MainTemplate';
 
 import styles from './styles.module.css';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { formatDate } from '../../utils/formatDate';
 
 export function History() {
   const { state } = useTaskContext();
@@ -45,7 +46,7 @@ export function History() {
                   <tr key={task.id}>
                     <td>{task.name}</td>
                     <td>{task.durationInMinutes}min</td>
-                    <td>{new Date(task.startDate).toISOString()}</td>
+                    <td>{formatDate(task.startDate)}</td>
                     <td>{task.interruptedDate}</td>
                     <td>{task.type}</td>
                   </tr>
