83f31f70ed53f3215c629c9ae32ed793c93fee35
unknown <jansevero@gmail.com>
2026-05-06 19:39:05 -0300

    Aula 95

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -7,9 +7,10 @@ import { MainTemplate } from '../../templates/MainTemplate';
 import { useRef } from 'react';
 import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
 import { toastifyAdapter } from '../../adapters/toastifyAdapter';
+import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
 
 export function Settings() {
-    const { state } = useTaskContext();
+    const { state, dispatch } = useTaskContext();
     const workTimeInput = useRef<HTMLInputElement>(null);
     const shortBreakTimeInput = useRef<HTMLInputElement>(null);
     const longBreakTimeInput = useRef<HTMLInputElement>(null);
@@ -34,7 +35,15 @@ export function Settings() {
             return;
         }
 
-        console.log('SALVAR');
+        dispatch({
+            type: TaskActionTypes.CHANGE_SETTINGS,
+            payload: {
+                workTime,
+                shortBreakTime,
+                longBreakTime,
+            },
+        });
+        toastifyAdapter.success('Configurações salvas');
     }
 
     return (
