091deaada4038bda7ddb5d0bde58843168a0f01a
unknown <jansevero@gmail.com>
2026-05-06 19:28:03 -0300

    Aula 94

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -4,41 +4,90 @@ import { DefaultButton } from '../../components/DefaultButton';
 import { DefaultInput } from '../../components/DefaultInput';
 import { Heading } from '../../components/Heading';
 import { MainTemplate } from '../../templates/MainTemplate';
+import { useRef } from 'react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { toastifyAdapter } from '../../adapters/toastifyAdapter';
 
 export function Settings() {
-  return (
-    <MainTemplate>
-      <Container>
-        <Heading>Configurações</Heading>
-      </Container>
+    const { state } = useTaskContext();
+    const workTimeInput = useRef<HTMLInputElement>(null);
+    const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+    const longBreakTimeInput = useRef<HTMLInputElement>(null);
 
-      <Container>
-        <p style={{ textAlign: 'center' }}>
-          Modifique as configurações para tempo de foco, descanso curto e
-          descanso longo.
-        </p>
-      </Container>
+    function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        toastifyAdapter.dismiss();
 
-      <Container>
-        <form action='' className='form'>
-          <div className='formRow'>
-            <DefaultInput id='workTime' labelText='Foco' />
-          </div>
-          <div className='formRow'>
-            <DefaultInput id='shortBreakTime' labelText='Descanso curto' />
-          </div>
-          <div className='formRow'>
-            <DefaultInput id='longBreakTime' labelText='Descanso longo' />
-          </div>
-          <div className='formRow'>
-            <DefaultButton
-              icon={<SaveIcon />}
-              aria-label='Salvar configurações'
-              title='Salvar configurações'
-            />
-          </div>
-        </form>
-      </Container>
-    </MainTemplate>
-  );
+        const formErrors = [];
+        const workTime = Number(workTimeInput.current?.value);
+        const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+        const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+        if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+            formErrors.push('Digite apenas números para TODOS os campos');
+        }
+
+        if (formErrors.length > 0) {
+            formErrors.forEach(error => {
+                toastifyAdapter.error(error);
+            });
+            return;
+        }
+
+        console.log('SALVAR');
+    }
+
+    return (
+        <MainTemplate>
+            <Container>
+                <Heading>Configurações</Heading>
+            </Container>
+
+            <Container>
+                <p style={{ textAlign: 'center' }}>
+                    Modifique as configurações para tempo de foco, descanso curto e
+                    descanso longo.
+                </p>
+            </Container>
+
+            <Container>
+                <form onSubmit={handleSaveSettings} action='' className='form'>
+                    <div className='formRow'>
+                        <DefaultInput
+                            id='workTime'
+                            labelText='Foco'
+                            ref={workTimeInput}
+                            defaultValue={state.config.workTime}
+                            type='number'
+                        />
+                    </div>
+                    <div className='formRow'>
+                        <DefaultInput
+                            id='shortBreakTime'
+                            labelText='Descanso curto'
+                            ref={shortBreakTimeInput}
+                            defaultValue={state.config.shortBreakTime}
+                            type='number'
+                        />
+                    </div>
+                    <div className='formRow'>
+                        <DefaultInput
+                            id='longBreakTime'
+                            labelText='Descanso longo'
+                            ref={longBreakTimeInput}
+                            defaultValue={state.config.longBreakTime}
+                            type='number'
+                        />
+                    </div>
+                    <div className='formRow'>
+                        <DefaultButton
+                            icon={<SaveIcon />}
+                            aria-label='Salvar configurações'
+                            title='Salvar configurações'
+                        />
+                    </div>
+                </form>
+            </Container>
+        </MainTemplate>
+    );
 }
