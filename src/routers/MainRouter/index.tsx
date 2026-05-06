6cdea68234640092656d6ea7059a02d478fdd1a5
unknown <jansevero@gmail.com>
2026-05-06 19:06:07 -0300

    Aula 92

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -4,6 +4,7 @@ import { NotFound } from "../../pages/NotFound";
 import { Home } from "../../pages/Home";
 import { useEffect } from "react";
 import { History } from '../../pages/History';
+import { Settings } from "../../pages/Settings";
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -21,8 +22,9 @@ export function MainRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/history/' element={<History />} />
         <Route path="/" element={<Home />} />
+        <Route path='/history/' element={<History />} />
+        <Route path='/settings/' element={<Settings />} />
         <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
