eefff82405766eaa8fbb889527b5ebdf07124f87
jansevero <jansevero@gmail.com>
2026-05-14 20:30:20 -0300

    aa

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ import { MainForm } from "../../components/MainForm";
 import { MainTemplate } from "../../templates/MainTemplate";
 
 export function Home() {
-//sw
+
   return (
     <MainTemplate>
       <Container>
